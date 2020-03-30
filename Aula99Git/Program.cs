fcbab363b98a342df7cf67bf5450cc2064883c4b
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2020-03-30 20:04:33 -0300

    Acrescentada uma mensagem de bom dia,

--- Aula99Git/Program.cs
+++ Aula99Git/Program.cs
@@ -7,6 +7,7 @@ namespace Aula99Git
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
         }
     }
 }
